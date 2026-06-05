2c21de5bb3d51e9d56e9b935d42b8d15ba60dc83
Anshul <se22uecm013@mahindrauniversity.edu.in>
2026-06-05 11:11:10 +0530

    Delete backend directory

--- backend/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from backend.auth.routes import router as auth_router
-from backend.rag.routes import router as rag_router
-
-app = FastAPI()
-
-app.include_router(auth_router)
-app.include_router(rag_router)
